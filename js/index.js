ba5cc5ec95483e9a5d75f7afb34711e4e0b0d53e
Red Chain Company <security@redchain.co>
2019-04-12 08:59:50 -0500

    servicios, jquery, img

---
+++ js/index.js
@@ -0,0 +1,31 @@
+// $(document).ready(function(){
+  $("#consulta-prioritaria").hover(function(){
+    $(".consulta-prioritaria").toggle()
+    // $("#imagen-diagnostica").toggle();
+    // $("#laboratorio-clinico").toggle();
+    // $("#electrocardiografia").toggle();
+    // $("#consulta-programada").toggle();
+    // $("#terapia-respiratoria").toggle();
+  });
+
+  $("#consulta-programada").hover(function(){
+    $(".consulta-programada").toggle();
+  });
+
+  $("#imagen-diagnostica").hover(function(){
+    $(".imagen-diagnostica").toggle();
+  });
+
+  $("#laboratorio-clinico").hover(function(){
+    $(".laboratorio-clinico").toggle();
+  });
+
+  $("#electrocardiografia").hover(function(){
+    $(".electrocardiografia").toggle();
+  });
+
+  $("#terapia-respiratoria").hover(function(){
+    $(".terapia-respiratoria").toggle();
+  });
+// })
+
---
+++ video/js/index.js
@@ -0,0 +1,5 @@
+// $(document).ready(function(){
+//   $(".fa-facebook").hover(function(){
+//     $("#facebook").show();
+//   });
+// });
