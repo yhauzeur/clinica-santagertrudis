1309af80cecf26c98cefb4a787291dc29646fb21
Red Chain Company <security@redchain.co>
2019-06-19 12:21:08 -0500

    adjust boxes

--- js/index.js
+++ js/index.js
@@ -1,76 +1,110 @@
 //. $(document).ready(function(){
   $("#consulta-prioritaria").click(function(){
     $(".consulta-prioritaria").toggle()
-    $(".consulta-prioritaria").css("width","100%")
 
-    // $("#consulta-programada").toggle();
-    // $("#imagen-diagnostica").toggle();
-    // $("#laboratorio-clinico").toggle();
-    // $("#electrocardiografia").toggle();
-    // $("#terapia-respiratoria").toggle();
+    $(".col-sm-6").css("max-width","100%");
+    $(".col-md-4").css("max-width","100%");
+    $(".col-lg-2").css("max-width","100%");
 
-    // $(".center-i-menu .container .row #consulta-prioritaria").removeClass('col-xs-12 col-sm-6 col-md-4 col-lg-2');
-    // $(this).addClass('col-xs-12');
+    $(".col-sm-6").css("flex","0px");
+    $(".col-md-4").css("flex","0px");
+    $(".col-lg-2").css("flex","0px");
+
+    $("#consulta-programada").toggle();
+    $("#imagen-diagnostica").toggle();
+    $("#laboratorio-clinico").toggle();
+    $("#electrocardiografia").toggle();
+    $("#terapia-respiratoria").toggle();
       });
 
   $("#consulta-programada").click(function(){
     $(".consulta-programada").toggle();
 
-    // $("#consulta-prioritaria").toggle();
-    // $("#imagen-diagnostica").toggle();
-    // $("#laboratorio-clinico").toggle();
-    // $("#electrocardiografia").toggle();
-    // $("#terapia-respiratoria").toggle();
-    // $(".center-i-menu .container .row #consulta-programada").removeClass('col-xs-12 col-sm-6 col-md-4 col-lg-2');
-    // $(this).addClass('col-xs-12');
+    $(".col-sm-6").css("max-width","100%");
+    $(".col-md-4").css("max-width","100%");
+    $(".col-lg-2").css("max-width","100%");
+
+    $(".col-sm-6").css("flex","0px");
+    $(".col-md-4").css("flex","0px");
+    $(".col-lg-2").css("flex","0px");
+
+    $("#consulta-prioritaria").toggle();
+    $("#imagen-diagnostica").toggle();
+    $("#laboratorio-clinico").toggle();
+    $("#electrocardiografia").toggle();
+    $("#terapia-respiratoria").toggle();
       });
 
   $("#imagen-diagnostica").click(function(){
     $(".imagen-diagnostica").toggle();
 
-    // $("#consulta-prioritaria").toggle();
-    // $("#consulta-programada").toggle();
-    // $("#laboratorio-clinico").toggle();
-    // $("#electrocardiografia").toggle();
-    // $("#terapia-respiratoria").toggle();
-    // $(".center-i-menu .container .row #imagen-diagnostica").removeClass('col-xs-12 col-sm-6 col-md-4 col-lg-2');
-    // $(this).addClass('col-xs-12');
+    $(".col-sm-6").css("max-width","100%");
+    $(".col-md-4").css("max-width","100%");
+    $(".col-lg-2").css("max-width","100%");
+
+    $(".col-sm-6").css("flex","0px");
+    $(".col-md-4").css("flex","0px");
+    $(".col-lg-2").css("flex","0px");
+
+    $("#consulta-prioritaria").toggle();
+    $("#consulta-programada").toggle();
+    $("#laboratorio-clinico").toggle();
+    $("#electrocardiografia").toggle();
+    $("#terapia-respiratoria").toggle();
       });
 
   $("#laboratorio-clinico").click(function(){
     $(".laboratorio-clinico").toggle();
 
-    // $("#consulta-prioritaria").toggle();
-    // $("#consulta-programada").toggle();
-    // $("#imagen-diagnostica").toggle();
-    // $("#electrocardiografia").toggle();
-    // $("#terapia-respiratoria").toggle();
-    // $(".center-i-menu .container .row #laboratorio-clinico").removeClass('col-xs-12 col-sm-6 col-md-4 col-lg-2');
-    // $(this).addClass('col-xs-12');
+    $(".col-sm-6").css("max-width","100%");
+    $(".col-md-4").css("max-width","100%");
+    $(".col-lg-2").css("max-width","100%");
+
+    $(".col-sm-6").css("flex","0px");
+    $(".col-md-4").css("flex","0px");
+    $(".col-lg-2").css("flex","0px");
+
+    $("#consulta-prioritaria").toggle();
+    $("#consulta-programada").toggle();
+    $("#imagen-diagnostica").toggle();
+    $("#electrocardiografia").toggle();
+    $("#terapia-respiratoria").toggle();
       });
 
   $("#electrocardiografia").click(function(){
     $(".electrocardiografia").toggle();
 
-    // $("#consulta-prioritaria").toggle();
-    // $("#consulta-programada").toggle();
-    // $("#imagen-diagnostica").toggle();
-    // $("#laboratorio-clinico").toggle();
-    // $("#terapia-respiratoria").toggle();
-    // $(".center-i-menu .container .row #electrocardiografia").removeClass('col-xs-12 col-sm-6 col-md-4 col-lg-2');
-    // $(this).addClass('col-xs-12');
+    $(".col-sm-6").css("max-width","100%");
+    $(".col-md-4").css("max-width","100%");
+    $(".col-lg-2").css("max-width","100%");
+
+    $(".col-sm-6").css("flex","0px");
+    $(".col-md-4").css("flex","0px");
+    $(".col-lg-2").css("flex","0px");
+
+    $("#consulta-prioritaria").toggle();
+    $("#consulta-programada").toggle();
+    $("#imagen-diagnostica").toggle();
+    $("#laboratorio-clinico").toggle();
+    $("#terapia-respiratoria").toggle();
       });
 
   $("#terapia-respiratoria").click(function(){
     $(".terapia-respiratoria").toggle();
 
-    // $("#consulta-prioritaria").toggle();
-    // $("#consulta-programada").toggle();
-    // $("#imagen-diagnostica").toggle();
-    // $("#laboratorio-clinico").toggle();
-    // $("#electrocardiografia").toggle();
-    // $(".center-i-menu .container .row #terapia-respiratoria").removeClass('col-xs-12 col-sm-6 col-md-4 col-lg-2');
-    // $(this).addClass('col-xs-12');
+    $(".col-sm-6").css("max-width","100%");
+    $(".col-md-4").css("max-width","100%");
+    $(".col-lg-2").css("max-width","100%");
+
+    $(".col-sm-6").css("flex","0px");
+    $(".col-md-4").css("flex","0px");
+    $(".col-lg-2").css("flex","0px");
+
+    $("#consulta-prioritaria").toggle();
+    $("#consulta-programada").toggle();
+    $("#imagen-diagnostica").toggle();
+    $("#laboratorio-clinico").toggle();
+    $("#electrocardiografia").toggle();
       });
 //. })
 
